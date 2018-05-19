dd13e4766c59b7e2042c6020b6df8a388b0ed551
Ankit Yadav <connectankityadav@gmail.com>
2018-05-19 23:50:17 +0530

    adds functionality to add tasks, debugs key issue

--- src/Checklist.js
+++ src/Checklist.js
@@ -5,7 +5,7 @@ class Checklist extends Component {
     render() {
 
         let tasks = this.props.tasks.map((task) => (
-            <li className="checklist__task">
+            <li key={task.id} className="checklist__task">
                 <input type="checkbox" defaultChecked={task.done} />
                 {task.name}
                 <a href="#" className="checklist__task--remove" />
@@ -16,6 +16,9 @@ class Checklist extends Component {
             <div className="checklist">
                 <ul>
                     {tasks}
+
+                    {/*Input Field for Creating a new task*/}
+                    <input type="text" className="checklist--add-task" placeholder="Type then hit enter to add a new task"/>
                 </ul>
             </div>
         );
--- src/List.js
+++ src/List.js
@@ -5,7 +5,8 @@ class List extends Component {
 
     render() {
         var cards = this.props.cards.map((card) => {
-            return <Card id={card.id}
+            return <Card key={card.id}
+                         id={card.id}
                          title={card.title}
                          description={card.description}
                          color={card.color}
